dcca1d5f80904bfcfcce6b2c891e8a70aa85452f
developerjannatmim <mim266758@gmail.com>
2023-08-15 22:03:14 +0600

    tutorial-4,5,6

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/signup', [UserController::class, 'store'])->name('signup');
-Route::get('/dashboard', [UserController::class, 'getdashboard']);
+
