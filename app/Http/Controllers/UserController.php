6baa4b5c844bc30f31a5551fdecd8481fef277fe
developerjannatmim <mim266758@gmail.com>
2023-08-16 10:26:51 +0600

    form-validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,19 +30,17 @@ class UserController extends Controller
     return view('dashboard');
   }
 
-  public function getSignIn(Request $request)
-  {
-    if  (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])){
-      return redirect()->route('dashboard');
-    };
-      return redirect()->back();
-  }
-
   /**
    * Store a newly created resource in storage.
    */
   public function getSignUp(Request $request)
   {
+    $this->validate($request, [
+      'first_name' => 'required',
+      'email' => 'required|email|max:120|unique:users',
+      'password' => 'required|min:4'
+    ]);
+
     $first_name = $request['first_name'];
     $email = $request['email'];
     $password = $request['password'];
@@ -54,8 +52,20 @@ class UserController extends Controller
 
     $user->save();
     Auth::login($user);
-    
+
     return redirect()->route('dashboard');
+  }
+
+  public function getSignIn(Request $request)
+  {
+    $this->validate($request, [
+      'email' => 'required',
+      'password' => 'required'
+    ]);
+    if  (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])){
+      return redirect()->route('dashboard');
+    };
+      return redirect()->back();
   }
 
   /**
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,32 @@ Welcome!
 @endsection
 
 @section('content')
+@if( count($errors) > 0 )
+<div class="row">
+  <div class="col-md-6">
+    <ul>
+      @foreach( $errors->all() as $error )
+      <li>{{$error}}</li>
+      @endforeach
+    </ul>
+  </div>
+</div>
+@endif
 <div class="row">
   <div class="col-md-6">
     <h3>Sign Up</h3>
     <form action="{{ route('signup') }}" method="POST">
-      <div class="form-group">
+      <div class="form-group {{ $errors->has('first_name') ? 'has-error' : '' }}">
         <label for="first_name" class="mb-2">First Name</label>
-        <input class="form-control" type="text" name="first_name" id="first_name" />
+        <input class="form-control" type="text" name="first_name" id="first_name" value="{{Request::old('first_name')}}" />
       </div>
-      <div class="form-group">
+      <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
         <label for="email" class="mb-2">Email</label>
-        <input class="form-control" type="text" name="email" id="email" />
+        <input class="form-control" type="text" name="email" id="email" value="{{Request::old('email')}}" />
       </div>
-      <div class="form-group">
+      <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
         <label for="password" class="mb-2">Password</label>
-        <input class="form-control" type="password" name="password" id="password" />
+        <input class="form-control" type="password" name="password" id="password" value="{{Request::old('password')}}" />
         <button type="submit" class="btn btn-primary mt-2">Submit</button>
         <input type="hidden" name="_token" value="{{ Session::token() }}"/>
       </div>
@@ -28,11 +39,11 @@ Welcome!
   <div class="col-md-6">
     <h3>Sign In</h3>
     <form action=" {{ route('signin') }} " method="POST">
-      <div class="form-group">
+      <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
         <label for="email" class="mb-2">Email</label>
         <input class="form-control" type="text" name="email" id="email" />
       </div>
-      <div class="form-group">
+      <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
         <label for="password" class="mb-2">Password</label>
         <input class="form-control" type="password" name="password" id="password" />
       </div>
