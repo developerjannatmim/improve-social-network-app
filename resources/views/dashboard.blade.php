3bf7bea24c8e893619059b7a3c5fb0f0499c2105
developerjannatmim <mim266758@gmail.com>
2023-08-16 17:41:12 +0600

    dashboard design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,44 @@
-<h1>The Dashboard</h1>
+@extends('layouts.master')
+
+@section('content')
+<section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h1>What do you have to say?</h1></header>
+            <form action="#">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="write something here..."></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary mt-2">Create Post</button>
+            </form>
+        </div>
+</section>
+<section class="row posts">
+  <div class="col-md-6 col-md-offset-3">
+    <header><h1>What other people say...</h1></header>
+    <article class="post">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, odit. Facere itaque hic vel impedit asperiores illum temporibus qui maxime, deserunt, corrupti culpa eos, ad sit similique omnis dolorem repellendus tempore! Nostrum ut nemo officiis itaque debitis possimus exercitationem explicabo architecto sunt! Expedita reprehenderit amet quis dolores, rerum cupiditate dolor?</p>
+      <div class="info">
+      Posted by Max on 16 December
+      </div>
+      <div class="interaction">
+      <a href="#">Like</a> |
+      <a href="#">Dislike</a> |
+      <a href="#">Edit</a> |
+      <a href="#">Delete</a> |
+      </div>
+    </article>
+    <article class="post">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, odit. Facere itaque hic vel impedit asperiores illum temporibus qui maxime, deserunt, corrupti culpa eos, ad sit similique omnis dolorem repellendus tempore! Nostrum ut nemo officiis itaque debitis possimus exercitationem explicabo architecto sunt! Expedita reprehenderit amet quis dolores, rerum cupiditate dolor?</p>
+      <div class="info">
+      Posted by Max on 17 December
+      </div>
+      <div class="interaction">
+      <a href="#">Like</a> |
+      <a href="#">Dislike</a> |
+      <a href="#">Edit</a> |
+      <a href="#">Delete</a> |
+      </div>
+    </article>
+  </div>
+</section>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link href="{{ URL::to('src/css/main.css') }}" rel="stylesheet" >
 </head>
 <body>
     @include('includes.header')
